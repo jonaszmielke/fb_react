3670fe767fa01cc31d062603f6d63ff9419ce913
Jonasz Mielke <168871782+jonaszmielke@users.noreply.github.com>
2025-01-12 01:10:17 +0100

    fixed userpage friends section and added mutual friends counter

---
+++ src/query/fetchuserdata.js
@@ -0,0 +1,23 @@
+const fetchUserData = async ({ queryKey, includeMutualFriends, jwt }) => {
+
+    const userid = queryKey[1];
+    let url = `http://localhost:3000/api/user/${userid}?includeMutualFriends=${includeMutualFriends}`;
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${jwt}`,
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch friends");
+    }
+
+    const result = await response.json();
+
+    return result;
+};
+
+export default fetchUserData;
--- src/user/user.jsx
+++ src/user/user.jsx
@@ -1,8 +1,12 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Cookies from 'js-cookie';
+
 import fetchUsersPosts from "../query/fetchusersposts";
 import fetchFriends from "../query/fetchfriends";
+import fetchUserData from "../query/fetchuserdata";
+
 import "./user.css";
 
 import Header from "../header";
@@ -10,6 +14,7 @@ import Post from "../post/post";
 
 const User = () => {
 
+    //authentication
     const userjwt = Cookies.get('userjwt');
     const { userid } = useParams();
 
@@ -17,59 +22,84 @@ const User = () => {
     const [userPosts, setUserPosts] = useState([]);
     const [depleted, setDepleted] = useState(false);
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    //user data
+    const { data: userData, isLoading: isUserDataLoading } = useQuery({
+        queryKey: ["userData", userid],
+        queryFn: () => fetchUserData({
+            queryKey: ["userData", userid],
+            includeMutualFriends: true,
+            jwt: userjwt
+        }),
+    });
 
-        if (!depleted) {
-            requestPosts(userPosts);
+    const displayMutualFriends = (userData, isUserDataLoading) => {
+        if (isUserDataLoading || !userData) {
+            return (
+                <div className="post">
+                    <p>...</p>
+                </div>
+            );
         }
 
-        const handleScroll = () => {
-            if (!depleted && window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-                requestPosts(userPosts);
-            }
-        };
+        return userData?.mutual_friends || 0;
+    };
 
-        window.addEventListener('scroll', handleScroll);
 
-        // Clean up scroll event listener
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [userPosts, depleted, userid]);
-
+    //getting posts
     async function requestPosts(currentPosts) {
         try {
             const new_posts = await fetchUsersPosts(userid, userjwt, currentPosts);
             console.log("requestPosts new_posts:", new_posts);
-
+    
             if (new_posts.allPostsDepleted) {
                 console.log('All posts depleted');
                 setDepleted(true);
                 return;
             }
-
+    
             setUserPosts((prevPosts) => [...prevPosts, ...new_posts.postids]);
         } catch (error) {
             console.error("Error fetching posts:", error);
         }
     }
 
+    //displaing posts
+    useEffect(() => {
 
-    //friends
+        setUserPosts([]);
+        setDepleted(false);
+    
+        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+        requestPosts([]);
+    
+        const handleScroll = () => {
+            if (!depleted && window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+                requestPosts([]);
+            }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [userid]); 
+
+
+    //getting friends
     const [friends, setFriends] = useState([]);
 
     async function requestFriends() {
-        try{
+        try {
             const result = await fetchFriends(userid, userjwt);
             console.log('friends:', result);
             setFriends(result);
-        } catch (error){
+        } catch (error) {
             console.error("Error fetching friends:", error);
         }
     }
 
     useEffect(() => {
         requestFriends();
-    }, []);
+    }, [userid, userjwt]);
 
 
     return (
@@ -80,11 +110,16 @@ const User = () => {
                     <section>tło</section>
                     <section>
                         <div className="profilePicture">
-                            <img src="" alt="profile picture" />
+                            <img
+                                src={userData?.profilePicture || "default-placeholder.png"}
+                                alt="profile picture"
+                            />
                         </div>
                         <div className="stats">
                             <h1>imie nazwisko</h1>
-                            <p>{friends.length} znajomi • x wspólni znajomi</p>
+                            <p>
+                                {friends.length} znajomi • {displayMutualFriends(userData, isUserDataLoading)} wspólni znajomi
+                            </p>
                         </div>
                         <div className="">znajomi button</div>
                     </section>
@@ -104,16 +139,28 @@ const User = () => {
                                 <p>Pokaż wszystkich znajomych</p>
                                 <p className="friendStats">{friends.length} (wspólnych: x)</p>
                             </div>
-                            <div>
-                                {friends.length > 0 ? friends.map((friend) => (
-                                    <div key={friend.id} className="friend">
-                                        <img 
-                                            src={`http://localhost:3000/app_images/profile_pictures/${friend.profilePictureUrl}`} 
-                                            alt="friend profile picture" 
-                                        />
-                                        <p>{friend.name} {friend.surname}</p>
-                                    </div>
-                                )) : <p>No friends to show</p>}
+                            <div className="friends-grid">
+                                {friends.length > 0 ? (
+                                    friends.map((friend) => (
+                                        <Link
+                                            to={`/user/${friend.id}`}
+                                            key={friend.id}
+                                            className="friend-link"
+                                        >
+                                            <div className="friend">
+                                                <img
+                                                    src={`http://localhost:3000/app_images/profile_pictures/${friend.profilePictureUrl}`}
+                                                    alt={`${friend.name} ${friend.surname}`}
+                                                />
+                                                <p>
+                                                    {friend.name} {friend.surname}
+                                                </p>
+                                            </div>
+                                        </Link>
+                                    ))
+                                ) : (
+                                    <p>No friends to show</p>
+                                )}
                             </div>
                         </div>
                     </div>
