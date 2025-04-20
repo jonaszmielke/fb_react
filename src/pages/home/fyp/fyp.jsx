b70a88d6a49fee0e37340666c36a87bb1a93212b
Jonasz Mielke <168871782+jonaszmielke@users.noreply.github.com>
2025-04-20 22:03:29 +0200

    Visual part of make post

--- src/pages/home/fyp/fyp.jsx
+++ src/pages/home/fyp/fyp.jsx
@@ -1,8 +1,9 @@
 //import { token } from "../login/login";
 import { useEffect, useState, useRef, useCallback } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuery ,useInfiniteQuery } from "@tanstack/react-query";
 import fetchForYouPage from "../../../query/fetchfyp";
 import Cookies from 'js-cookie';
+import fetchUserData from "../../../query/user/fetchuserdata";
 
 import "./fyp.css";
 
@@ -12,6 +13,7 @@ import Post from "../../../components/post/post";
 
 const ForYouPage = () => {
     const userjwt = Cookies.get('userjwt');
+    const user = JSON.parse(Cookies.get('user'));
     const {
         data : postsData,
         fetchNextPage,
@@ -42,12 +44,36 @@ const ForYouPage = () => {
     }, [isLoading, hasNextPage]);
 
 
+    const { data: userData, isLoading: isUserDataLoading, isError: isUserDataError } = useQuery({
+        queryKey: ["userData", parseInt(user.id)],
+        queryFn: ({ queryKey }) => {
+            return fetchUserData({ queryKey, userjwt });
+        }
+    });
+
+
     return (
         <div id="main">
             <Header selected={1}/>
             <div className="body_fyp">
                 <section></section>
                 <section id="posts" className="posts">
+
+                    <div className="make_post">
+
+                        <img 
+                            className="headerProfilePicture"
+                            src={`http://localhost:3000/app_images/profile_pictures/${isUserDataLoading || isUserDataError ? 'default.jpg' : userData.profile_picture_url}`} 
+                        />
+                        <p
+                            className="make_post_input"
+                            onClick={() => {}}
+                        >
+                            What are you thinking about, {userData.name}?
+                        </p>
+
+                    </div>
+
                     { isLoading ? 'Loading' :
                         isError ? 'Error' : 
                             posts.map((id, index) => {
